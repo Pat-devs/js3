2663f85701b9626a38acd97c96d3d6155b67223b
pat-devs <patryk.kodehode@gmail.com>
2024-11-21 10:27:48 +0100

    examples of using return statements

--- script.js
+++ script.js
@@ -7,25 +7,26 @@ function greet() {
 }
 
 // function that says a custom greeting in the console
-function customGreet(abcdef) {
+function customGreet(greeting) {
     //let message = parameter
-    console.log(abcdef)
+    //console.log(greeting)
+    return greeting
 }
 
 //customGreet("Hej")
 
 
-// sum takes two numbers and prints out the sum
-function sum(numberA, numberB) {
+// sum takes two numbers and prints out the sum in the console
+function sumConsole(numberA, numberB) {
     let sum = numberA + numberB
 
     console.log("The sum is " + sum)
 }
 
-//sum(2, 3)
+//sumConsole(2, 3)
 
-// sub takes two numbers and prints out the substraction result
-function sub(numberA, numberB) {
+// sub takes two numbers and prints out the substraction result in the console
+function subConsole(numberA, numberB) {
     let result = numberA - numberB
 
     //console.log(`The result is ${result}`)
@@ -33,14 +34,50 @@ function sub(numberA, numberB) {
     console.log(`The result is ${numberA - numberB}`)
 }
 
-//sub(2, 3)
+//subConsole(2, 3)
 
 
-// sum takes two numbers and prints out the sum
+// Html tags
+let calcElement = document.querySelector("#calc") // document.getElementById("calc")c
+
+
+// sum takes two numbers and displays the sum on the webpage
 function sumHtml(numberA, numberB) {
     let sum = numberA + numberB
 
-    document.body.textContent = "The sum is " + sum
+    calcElement.textContent = "The sum is " + sum
+    //document.body.textContent = "The sum is " + sum
 }
 
-sumHtml(5, 50)
+//sumHtml(5, 50)
+
+
+
+// wishlist:
+
+// #1. i wish to have a function that can take two numbers as arguments, and then return their sum
+// #2. i wish to hava a function that can display the given argument on the webpage
+// #3. i wish to hava a function that can display the given argument on the console
+
+function sum(numberA, numberB) {
+    let sum = numberA + numberB
+
+    return sum
+}
+
+//console.log(sum(2,34))
+
+//calcElement.textContent = sum(2,34)
+
+// display data, displays the given argument on the webpage
+// specifically on the p-tag with id calc
+function displayData(someData) {
+    calcElement.textContent = someData
+}
+
+
+displayData("the sum of 2 and 34 is " + sum(2,34))
+
+console.log(
+    sum(2,34)
+)
