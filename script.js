ea11f5a110926c981c008b56b83091366249bf9d
pat-devs <patryk.kodehode@gmail.com>
2024-11-22 09:33:24 +0100

    for loops

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if ("noe condition som må være true") {
 
 // While loop
 
-let lapsCompleted = 0
+/* let lapsCompleted = 0
 
 // run 10 loops:
 while (lapsCompleted < 10) {
@@ -25,4 +25,25 @@ while (lapsCompleted < 10) {
     if (lapsCompleted > 5) {
         break
     }
+} */
+
+// For loop
+
+for (let lapsCompleted = 0; lapsCompleted < 10; lapsCompleted += 1) {
+
+    // console.log("lap number: " + lapsCompleted)
+}
+
+// for (declare a variable; "create a condition"; increment OR decrement the variable)
+
+// count 0 to 5 (inclusive) in the console
+for (let i = 0; i <= 5; i++) {
+    //console.log("count is now: " + i)
+}
+
+// count 50 to 10 (inclusive) in the console
+for (let i = 50; i >= 10; i--) {
+    //console.log("count is now: " + i)
+
+    break
 }
